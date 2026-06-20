77755004e587799b928cf0bce7591d30cf558227
Ahmed Mhedhbi <102544005+AhmedMhedhbi00@users.noreply.github.com>
2026-06-20 10:55:29 +0200

    modifica effettuata

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -476,12 +476,7 @@ $dropProdotti = isset($prodottiLeVrai) && $prodottiLeVrai->count() > 0
             <img src="{{ asset('assets/images/logo-nike.png') }}" alt="Nike">
             <img src="{{ asset('assets/images/logo-nocta.png') }}" alt="Nocta">
             <img src="{{ asset('assets/images/Logo_Adidas.png') }}" alt="Adidas">
-                    <img src="{{ asset('assets/images/logo-nike.png') }}" alt="Nike">
-            <img src="{{ asset('assets/images/logo-nocta.png') }}" alt="Nocta">
-            <img src="{{ asset('assets/images/Logo_Adidas.png') }}" alt="Adidas">
-            <img src="{{ asset('assets/images/logo-nike.png') }}" alt="Nike">
-            <img src="{{ asset('assets/images/logo-nocta.png') }}" alt="Nocta">
-            <img src="{{ asset('assets/images/Logo_Adidas.png') }}" alt="Adidas">
+
         </div>
     </div>
 </section>
